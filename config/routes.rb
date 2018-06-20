9d8596c757490d6b07f63d4a131204ef9fc96f7b
Théo Duranti <theoduranti@hotmail.fr>
2018-06-20 15:26:37 +0200

    devise pro et ele avec registrations_ele et pro

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :pros, :controllers => { registrations: 'registrations_pro' }
+  devise_for :eles, :controllers => { registrations: 'registrations_ele' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
